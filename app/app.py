1cf0ef7fb2e7eadfcbc64eedc47f9df3fbb379f2
Sebaztian-Hane <gonzalesvsll106@gmail.com>
2025-07-06 14:41:26 -0700

    feat:añado funcionalidad(backend)

---
+++ app/app.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template,request, jsonify
+from deep_translator import GoogleTranslator
+import requests
+
+app = Flask(__name__, template_folder='templates')
+
+@app.route('/definir')
+def definir_palabra():
+    palabra_es = request.args.get('palabra', '').strip().lower()
+    
+    if not palabra_es:
+        return jsonify({"error": "Debes ingresar una palabra"}), 400
+    
+    try:
+        #Español → Inglés
+        palabra_en = GoogleTranslator(source='es', target='en').translate(palabra_es)
+        
+        #Consultar API inglesa
+        api_url = f"https://api.dictionaryapi.dev/api/v2/entries/en/{palabra_en}"
+        respuesta = requests.get(api_url, timeout=5).json()        
+        definicion_en = respuesta[0]['meanings'][0]['definitions'][0]['definition']
+        
+        #Inglés → Español
+        definicion_es = GoogleTranslator(source='en', target='es').translate(definicion_en)
+        
+        return jsonify({
+            "palabra": palabra_es,
+            "definicion": definicion_es
+        })
+    
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+    
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
